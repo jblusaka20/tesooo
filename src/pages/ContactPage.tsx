f5f509bc7e66c207a237de11b9f6568c396066df
jblusaka20 <jblusaka20@gmail.com>
2025-10-24 14:11:22 +0200

    Updated ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -238,7 +238,7 @@ const ContactPage: React.FC = () => {
               <div className="w-full h-64">
                   <iframe
                     className="w-full h-full rounded-lg"
-                    src="https://www.google.com/maps/search/Pelican+Street,+Salama+Park,+Lusaka,+Zambia/@-15.3951995,28.3826991,17z/data=!3m1!4b1?entry=ttu&g_ep=EgoyMDI1MTAyMC4wIKXMDSoASAFQAw%3D%3D"
+                    src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3846.6542640671664!2d28.382699075293374!3d-15.39519951325258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPelican%20Street%2C%20Salama%20Park%2C%20Lusaka%2C%20Zambia!5e0!3m2!1sen!2szm!4v1761307742555!5m2!1sen!2szm"
                     width="100%"
                     height="100%"
                     style={{ border: 0 }}
